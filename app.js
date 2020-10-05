3f5da4beb3de8ba32606da81f43f3bbe6e7c2983
veetiveeti <veeti.v.reponen@gmail.com>
2020-10-05 12:15:34 +0300

    Created displayCurrentTime function
    – Updates time of DOM element every second

--- app.js
+++ app.js
@@ -18,57 +18,128 @@
 11. Service now fetches API data, console log works.
 */
 
-window.addEventListener("load", ()=> {
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector(".card__temperature__description");
-    let temperatureDegree = document.querySelector(".card__temperature__degree");
-    let feelsLike = document.querySelector(".card__temperature__feelslike");
-    let locationName = document.querySelector(".card__location__name");
-    let celsius = ('°C');
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".card__temperature__description"
+  );
+  let temperatureDegree = document.querySelector(".card__temperature__degree");
+  let feelsLike = document.querySelector(".card__temperature__feelslike");
+  let locationName = document.querySelector(".card__location__name");
+  let celsius = "°C";
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position =>{
-            console.log(position); /*log current position*/
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position); /*log current position*/
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=c6d3b1d2efe6a958b08601d6f711ba70&units=metric`;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=c6d3b1d2efe6a958b08601d6f711ba70&units=metric`;
 
-            fetch(api)
-             .then(response => {
-                 return response.json();
-             })
-             .then(data => {
-                 console.log(data); /* Logs data after it was fetched from API */
-                 const {name} = data;
-                 const {temp} = data.main; /* Within brackets can define what part of data.main you want*/
-                 const {main} = data.weather[0]; /* Within brackets can define what part of data.weather (first array) you want */
-                 const {feels_like} = data.main;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data); /* Logs data after it was fetched from API */
+          const { name } = data;
+          const {
+            temp,
+          } = data.main; /* Within brackets can define what part of data.main you want*/
+          const {
+            main,
+          } = data.weather[0]; /* Within brackets can define what part of data.weather (first array) you want */
+          const { feels_like } = data.main;
 
-                 console.log(name);
-                 console.log(temp); /* This prints temperature of the current location */
-                 console.log(main); /* This prints summary of the weather (sunny, cloudy, rain etc) */
-                 console.log(feels_like);
+          console.log(name);
+          console.log(
+            temp
+          ); /* This prints temperature of the current location */
+          console.log(
+            main
+          ); /* This prints summary of the weather (sunny, cloudy, rain etc) */
+          console.log(feels_like);
 
-                 //Set DOM elements from the API
-                 locationName.textContent = name;
-                 temperatureDegree.textContent = Math.round(temp)+celsius;
-                 feelsLike.textContent = ('Feels like ') + Math.round(feels_like);
-                 temperatureDescription.textContent = main;  
-                            
-             });
-        
-        // Get dates for card__location__date
-        const date = new Date();
-        const today = date.getDate();
-        const currentMonth = date.getMonth();
-        let locationDate = document.querySelector(".card__location__date"); 
-
-        //Set DOM elements
-        locationDate.textContent = today + ('.') + currentMonth + ('.');
-        
+          //Set DOM elements from the API
+          locationName.textContent = name;
+          temperatureDegree.textContent = Math.round(temp) + celsius;
+          feelsLike.textContent =
+            "Feels like " + Math.round(feels_like) + celsius;
+          temperatureDescription.textContent = main;
         });
+    });
+  }
+
+  /* Get dates for card__location__date
+  let currentDate = new Date();
+  let today = currentDate.getDate();
+  let month = currentDate.getMonth();
+  let locationDate = document.querySelector(".card__location__date"); */
+
+  //Set DOM element
+  //locationDate.textContent = today + "." + month + ".";
+
+  // Gets current time for card__location__time
+  function displayCurrentTime() {
+    let currentTime = new Date();
+    let today = currentTime.getDate();
+    let month = currentTime.getMonth();
+
+    today = addZero(today);
+    month = addZero(month);
+
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    
+    hours = addZero(hours);
+    minutes = addZero(minutes);
+    seconds = addZero(seconds);
+
+    let locationTime = document.querySelector(".card__location__time");
+
+    let timeString = ` ${today}.${month}. ${hours}:${minutes}:${seconds} `;
+
+    //Set DOM element
+    locationTime.textContent = timeString;
+
+    let timer = setTimeout(displayCurrentTime, 500);
+    console.log(timer);
+
+  }
+
+  //Adds 0 before single digit date and time
+  function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
     }
+    return i;
+  }
+
+  displayCurrentTime();
+
+  /*
+  function startClock() {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    let locationTime = document.querySelector(".card__location__time");
+    //Set DOM elements
+    locationTime.textContent = hours + ":" + minutes + ":" + seconds;
+
+    setTimeout(startClock, 500);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  startClock();*/
 });
 
